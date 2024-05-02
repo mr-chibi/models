a5b65ba78edd15c956d22306c14741899b0cf0b4
Alex <59456745+mr-chibi@users.noreply.github.com>
2024-05-01 21:42:41 -0400

    Wiki Pages Finished

---
+++ js/return_to_top.js
@@ -0,0 +1,7 @@
+let fetch_wrapper = document.getElementsByClassName("wrapper");
+let total_wrappers = fetch_wrapper.length;
+
+if(total_wrappers > 2) {
+
+	document.write("<a href=\"#top\" id=\"return_btn\">&uarr;</a>");
+}
